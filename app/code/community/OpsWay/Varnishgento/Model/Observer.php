0e19cb2fb7c5e5ddd5ba9fc3d6bced1913d35103
Alexandr Vronskiy <shandy.developer@gmail.com>
2014-06-03 13:15:40 +0300

    [Issue #12] Compatible with magento CE1.8 EE1.13. Csrf disabled if ajax is disabled too.

--- app/code/community/OpsWay/Varnishgento/Model/Observer.php
+++ app/code/community/OpsWay/Varnishgento/Model/Observer.php
@@ -381,4 +381,28 @@ class OpsWay_Varnishgento_Model_Observer
         }
     }
 
+    public function disableCsrf($observer)
+        {
+            if (!$this->_helper->getAjaxifyHelper()->isActive()){
+                $events = array(
+                    'checkout_cart_add',
+                    'checkout_cart_addgroup',
+                    'checkout_cart_updatepost',
+                    'review_product_post',
+                    'sendfriend_product_sendmail',
+                    'wishlist_index_add',
+                    'wishlist_index_update',
+                    'wishlist_index_cart',
+                    'wishlist_index_send',
+                    'catalog_product_compare_add',
+                );
+                $route = $observer->getEvent()->getControllerAction()->getFullActionName();
+
+                if (in_array($route, $events)) {
+                    $key = Mage::getSingleton('core/session')->getFormKey();
+                    Mage::app()->getRequest()->setParam('form_key', $key);
+                }
+            }
+        }
+
 }
